370f8a6528f2cf19263b449327d910be728c09c8
Aurélien Matouillot <a.matouillot@gmail.com>
2017-04-26 07:46:34 +0200

    Execute code in the right folder
    Also add missing lib

--- angular/build.sh
+++ angular/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+# Be sure we run the script in it's folder
+cd $SCRIPTPATH
+
+rm -rf build/
+
 npm run build:aot
 
 # NOTE: we keep the path in node_modules to not have to update the index.html file
@@ -11,9 +19,10 @@ mkdir -p build/node_modules/core-js/client/
 cp node_modules/core-js/client/shim.min.js build/node_modules/core-js/client/
 mkdir -p build/node_modules/zone.js/dist/
 cp node_modules/zone.js/dist/zone.js build/node_modules/zone.js/dist/
+mkdir -p build/node_modules/google-fonts
+cp google-fonts/2fcrYFNaTjcS6g4U3t-Y5StnKWgpfO2iSkLzTz-AABg.ttf build/node_modules/google-fonts/
 cp src/styles.css build/
 cp src/index-aot.html build/index.html
 
-rm -f pymotion.zip
 cd build
-zip -r ../pymotion.zip *
+zip -r pymotion-ng.zip *
